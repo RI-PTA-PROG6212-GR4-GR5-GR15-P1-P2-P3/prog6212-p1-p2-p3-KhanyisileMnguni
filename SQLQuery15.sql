350a56eefcd128bc7f730fbed870ab6fb7a443ca
KhanyisileMnguni <ST10476585@rcconnect.edu.za>
2026-09-02 15:53:27 +0200

    Correct typos and add new events to SQL insert
    Fixed typos in event descriptions and added new events.

--- SQLQuery15.sql
+++ SQLQuery15.sql
@@ -7,4 +7,6 @@ values('Thandiwe Dube', 'thandidube@gmail.com', 'hashed_pw_3', '1995-05-24', '08
 ('James Botha', 'james@gmail.com', 'hashed_pw_4', '1988-12-03', '063 023 4589');
 
 insert into Event(OrganiserID, EventName,EventDate,Location,Description)
-values(1,'Centurion Sunrise Run','2026-10-12','Centurion, Gauteng','A community road ruuning event through Centurion'),
+values(1,'Centurion Sunrise Run','2026-10-12','Centurion, Gauteng','A community road ruuning event through Centurion.'),
+(2,'Heritage Charity Walk','2026-09-25','Pretoria,Gauteng','Charity walk raising funds for local schools.'),
+(3,'Cape Coastal Cycle Challenge','2026-11-12','Cape Twon, Western CApe','A scenic cyclying event along the Cape coastline.');
